d7878d3d2bdbef034a65a78a95b5d7f50a476bed
Lavina Wadhwani <laviwadhwani12@gmail.com>
2026-06-28 20:23:37 +0530

    Assign 1 uploaded

---
+++ dailyAssignments/src/dailyAssignments/Assignment_1.java
@@ -0,0 +1,19 @@
+package dailyAssignments;
+
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+		
+		int studentId=101;
+		System.out.println("Student Id = "+ studentId);
+		
+		char grade='A';
+		System.out.println("Grade = "+grade);
+		
+		boolean passed=true;
+		System.out.println("Passed = "+passed);
+		
+		int age = 18;
+		System.out.println("Age = "+age);
+	}
+}
